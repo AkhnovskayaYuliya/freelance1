ba5866732cf61ae75f0e3220dddcf6c2b72de26c
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-08-16 16:34:55 +0300

    two

--- src/test/java/ru/netology/lya/freelance/services/WorkServiceTest.java
+++ src/test/java/ru/netology/lya/freelance/services/WorkServiceTest.java
@@ -2,31 +2,54 @@ package ru.netology.lya.freelance.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class WorkServiceTest {
-    @Test
-    void Test1() {
-        WorkService service = new WorkService();
-        int income = 10000;
-        int expences = 3000;
-        int threshold = 20000;
-        int expected = 3;
-        int actual = service.calcService(income, expences, threshold);
+    // @Test
+    // public void Test1() {
+    // WorkService service = new WorkService();
+    // int income = 10000;
+    // int expences = 3000;
+    // int threshold = 20000;
+    // int expected = 3;
+    //  int actual = service.calcService(income, expences, threshold);
+    //  Assertions.assertEquals(expected, actual);
+    //  }
 
+    //@Test
+    //public void Test2() {
+    //WorkService service = new WorkService();
+    // int income = 100000;
+    //int expences = 60000;
+    //int threshold = 150000;
+    //int expected = 2;
+    //int actual = service.calcService(income, expences, threshold);
+
+    //Assertions.assertEquals(expected, actual);
+    //}
+//}
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+
+    })
+    public void Test3(int expected, int income, int expences, int threshold) {
+        WorkService service = new WorkService();
+        int actual = service.calcService(income, expences, threshold);
         Assertions.assertEquals(expected, actual);
+
     }
 
-    @Test
-    void Test2() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/work.csv")
+    public void Test4(int expected, int income, int expences, int threshold) {
         WorkService service = new WorkService();
-        int income = 100000;
-        int expences = 60000;
-        int threshold = 150000;
-        int expected = 2;
         int actual = service.calcService(income, expences, threshold);
-
         Assertions.assertEquals(expected, actual);
+
     }
 }
-
